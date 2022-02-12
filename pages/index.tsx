44c2fcf99fddadc9ef33355a3735e5656994f8ff
jakequinter <hello@jakequinter.io>
2022-02-12 12:36:30 -0500

    init typescript and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-neutral-50 h-screen flex justify-center items-center">
+      <h1 className="text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-fuchsia-500 to-pink-400">
+        Coming soon...
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
