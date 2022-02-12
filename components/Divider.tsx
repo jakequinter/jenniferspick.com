e69a2858b06aa142e227bfd13f95c50fa819c15a
jakequinter <hello@jakequinter.io>
2022-02-12 12:27:03 -0500

    clean slate

--- components/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Divider = () => <hr className="border-t-2 border-gray-200 my-16" />;
-
-export default Divider;
--- lib/db.ts
+++
@@ -1,8 +0,0 @@
-import firebase from './firebase';
-import 'firebase/firestore';
-
-const firestore = firebase.firestore();
-
-export function createItem(data) {
-  return firestore.collection('items').add(data);
-}
